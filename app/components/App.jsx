3f2659fc779b55ecd56822e16af47aa081e9cade
Ri <ri.le@upstatement.com>
2021-03-22 17:20:53 -0400

    added app component

---
+++ app/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Notes from './Notes';
+import uuid from 'uuid';
+
+const notes = [
+  {
+    id: uuid.v4(),
+    task: 'Learn React'
+  },
+  {
+    id: uuid.v4(),
+    task: 'Do laundry'
+  }
+];
+export default () => <Notes notes={notes} />;
---
+++ app/components/Notes.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default ({ notes }) => (
+  <ul>{notes.map(note =>
+    <li key={note.id}>{note.task}</li>
+  )}</ul>
+)
